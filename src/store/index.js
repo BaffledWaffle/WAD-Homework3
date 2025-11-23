32ec16ffa3fd6c44e6579e82aade603d234e2f8c
NeleTaisi <nele.taisi@gmail.com>
2025-11-23 18:06:10 +0200

    Sign up page changes
    Added css file for signup page.
    Made some changes in footer.vue file.
    Added actions to index.js file in store folder.

--- src/store/index.js
+++ src/store/index.js
@@ -8,6 +8,41 @@ export default createStore({
   mutations: {
   },
   actions: {
+    // Method for checking, if the password is valid (meets all the requirements)
+    async validatePassword(context, password) {
+      const errors = [];
+
+      if (password.length < 8 || password.length > 15) {
+        errors.push("be 8–15 characters long");
+      }
+
+      if (!/^[A-Z]/.test(password)) {
+        errors.push("start with an uppercase letter");
+      }
+
+      if (!/[A-Z]/.test(password)) {
+        errors.push("include one uppercase letter");
+      }
+
+      if ((password.match(/[a-z]/g) || []).length < 2) {
+        errors.push("include two lowercase letters");
+      }
+
+      if (!/[0-9]/.test(password)) {
+        errors.push("include one number");
+      }
+
+      if (!/[_]/.test(password)) {
+        errors.push('include the character "_"');
+      }
+
+      return errors;
+    },
+
+    async signup({ dispatch }, { password }) {
+      const errors = dispatch("validatePassword", password);
+      return errors;
+    }
   },
   modules: {
   }
